10d6a7c23cb664d608f6483140306d552ef1fdc0
ricardopp1992 <ricardopp1992@gmail.com>
2019-04-29 14:41:01 -0300

    Perimera entrega Login

--- login.php
+++ login.php
@@ -20,14 +20,14 @@
         <div class="card card-signin my-5">
           <div class="card-body">
             <h5 class="card-title text-center"> <i class="fas fa-shopping-cart"></i> Compra-rapido.com</h5>
-            <form class="form-signin">
+            <form class="form-signin" action="./loguearUsuario.php" method="post">
               <div class="form-label-group">
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                <input name="usuario" type="text" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
                 <label for="inputEmail">Usuario</label>
               </div>
 
               <div class="form-label-group">
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
                 <label for="inputPassword">Contraseña</label>
               </div>
 
