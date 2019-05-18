c646d0f5215f8f45cfcd7a66f7791631c65ce3a7
Rodrigo Sosa <50084170+rodrigososa21@users.noreply.github.com>
2019-05-18 14:06:33 -0300

    Add files via upload
    Maquetado + correcciones

---
+++ clases/Credenciales.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('./DB/ManejadorDB.php');
+
+class Credenciales{
+    private $usuario;
+    private $password;
+
+    public function __construct($usuario, $password){
+        $this->usuario = $usuario;
+        $this->password = $password;
+    }
+
+    public function tipoDeCredenciales(){
+        $conexion = new ManejadorDB();
+
+        return $conexion->buscarUsario($this->usuario, $this->password);
+    }
+}
---
+++ loguearUsuario.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('./clases/Credenciales.php');
+require_once('./clases/Loguear.php');
+
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+
+$credenciales = new Credenciales($usuario, $password);
+$tipoDeCredenciales = $credenciales->tipoDeCredenciales();
+
+$login = new Loguear();
+
+$usuario = $login->loguearUsuario($tipoDeCredenciales);
+
+$usuario->redireccionar();
