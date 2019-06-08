f04e7006e7dca145fc8c5c04c239da2f7d6451ee
rodrigososa21 <rodriisosa21@gmail.com>
2019-06-08 19:39:30 -0300

    Empezando proceso de publicación de un producto

--- app/controller/controller_agregar_articulo.php
+++ app/controller/controller_agregar_articulo.php
@@ -9,5 +9,40 @@
         public function index(){
             $this->view->generate("agregar_articulo_view.php", "template_view.php");
         }
+
+        public function agregar(){
+        	if($_SERVER['REQUES_METHOD'] == 'POST'){
+        		$inputsValidos = $this->validarInputPost($_POST);
+
+        		if($inputsValidos){
+        			$this->model->agregarProducto($_POST);
+
+        		} else {
+        			$this->view->generate("agregar_articulo_view.php", "template_view.php", $inputsErroneos);
+        		}
+        	}
+        }
+
+        public function validarInputPost($post){
+        	$inputPost = $post;
+        	$inputsRequeridos = ["nombre", "descripcion", "precio", "cantidad", "categoria", "fecha_publicacion"];
+        	$inputsErroneos;
+        	$i = 0;
+
+        	foreach ($inputPost as $key => $value) {
+        		if($value != ""){
+        			$inputsErroneos[$i] = $key;
+        			$i++;
+        		}
+        	}
+
+        	if(sizeof($inputsErroneos) > 0){
+        		return false;
+
+        	} else {
+        		// validamos los campos criticos.
+        		return true;
+        	}
+        }
     }
 ?>
--- app/view/agregar_articulo_view.php
+++ app/view/agregar_articulo_view.php
@@ -1,88 +1,88 @@
 <div class="row align-items-center">
-  <div class="col-12">
-    <div class="col-sm-9 col-md-7 col-lg-7 mx-auto">
-      <div class="card card-signin my-5">
-        <div class="card-body">
-				<h4 class="text-center">Pon tu anuncio gratis</h4>
-				<br>
-				<form class="form-signin" enctype="multipart/form-data">
-				<h4 class="text-info">Detalles de tu anuncio</h4>
-					<div class="form-group">
-						<label class="col-sm-3">Código postal</label>
+  	<div class="col-12">
+    	<div class="col-sm-9 col-md-7 col-lg-7 mx-auto">
+      		<div class="card card-signin my-5">
+       			<div class="card-body">
+					<h4 class="text-center">Pon tu anuncio gratis</h4>
+					<br>
+					<form class="form-signin" enctype="multipart/form-data">
+						<h4 class="text-info">Detalles de tu anuncio</h4>
+						<div class="form-group">
+							<label class="col-sm-3">Código postal</label>
 							<div class="col-sm-7">
 								<input type="text" class="form-control" placeholder="Código postal" required autofocus>
 							</div>
-          </div>
-          <div class="form-group">
-						<label class="col-sm-3">Título del anuncio</label>
+          				</div>
+          				<div class="form-group">
+							<label class="col-sm-3">Título del anuncio</label>
 							<div class="col-sm-7">
 								<input type="text" class="form-control" placeholder="Título del anuncio" required autofocus>
 							</div>
-          </div>
-					<div class="form-group">
-						<label class="col-sm-3">Descripción</label>
+          				</div>
+						<div class="form-group">
+							<label class="col-sm-3">Descripción</label>
 							<div class="col-sm-7">
 								<textarea class="form-control" rows="3" placeholder="Descripción" required autofocus></textarea>
 							</div>
-          </div>
-					<div class="form-group">
-						<label class="col-sm-3">Precio</label>
+          				</div>
+						<div class="form-group">
+							<label class="col-sm-3">Precio</label>
 							<div class="col-sm-7">
 								<input type="text" class="form-control" placeholder="Precio" required autofocus>
 							</div>
-          </div>
-					<br>
-					<h4 class="text-info">Foto</h4>
-					<p>¡Los anuncios con fotos reciben 7 veces más contactos!</p>
-					<div class="form-group">
-						<div class="text-muted text-center">
-								<input type="file" multiple="multiple" min="1" max="10"/>
-						</div>
+          				</div>
 						<br>
-						<p class="text-muted text-center">¡Selecciona hasta 10 fotos! <i class="far fa-images"></i></p>
-					</div>
-					<h4 class="text-info">Datos para que te contacten</h4>
-					<div class="form-group">
-						<label class="col-sm-3">Eres</label>
-						<div class="col-sm-7">
-							<label>
-								<input type="checkbox" value="option1"> Particular
-							</label>
-							<label>
-								<input type="checkbox" value="option2"> Profesional
-							</label>
+						<h4 class="text-info">Foto</h4>
+						<p>¡Los anuncios con fotos reciben 7 veces más contactos!</p>
+						<div class="form-group">
+							<div class="text-muted text-center">
+								<input type="file" multiple="multiple" min="1" max="10"/>
+							</div>
+							<br>
+							<p class="text-muted text-center">¡Selecciona hasta 10 fotos! <i class="far fa-images"></i></p>
 						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-3">Tu nombre</label>
-						<div class="col-sm-7">
-						    <input type="text" class="form-control" placeholder="Nombre">
+						<h4 class="text-info">Datos para que te contacten</h4>
+						<div class="form-group">
+							<label class="col-sm-3">Eres</label>
+							<div class="col-sm-7">
+								<label>
+									<input type="checkbox" value="option1"> Particular
+								</label>
+								<label>
+									<input type="checkbox" value="option2"> Profesional
+								</label>
+							</div>
 						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-3">Email</label>
-						<div class="col-sm-7">
-						    <input type="email" class="form-control" placeholder="Email">
+						<div class="form-group">
+							<label class="col-sm-3">Tu nombre</label>
+							<div class="col-sm-7">
+						    	<input type="text" class="form-control" placeholder="Nombre">
+							</div>
 						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-3">Repetir Email</label>
-						<div class="col-sm-7">
-						    <input type="email" class="form-control" placeholder="Repetir Email">
+						<div class="form-group">
+							<label class="col-sm-3">Email</label>
+							<div class="col-sm-7">
+						    	<input type="email" class="form-control" placeholder="Email">
+							</div>
 						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-3">Teléfono</label>
-						<div class="col-sm-7">
-						    <input type="tel" class="form-control" placeholder="Teléfono">
+						<div class="form-group">
+							<label class="col-sm-3">Repetir Email</label>
+							<div class="col-sm-7">
+						    	<input type="email" class="form-control" placeholder="Repetir Email">
+							</div>
 						</div>
-					</div>
-					<p class="text-center">
-						<button class="btn btn-lg btn-secondary" type="submit">Continuar</button>
-					</p>
+						<div class="form-group">
+							<label class="col-sm-3">Teléfono</label>
+							<div class="col-sm-7">
+						    	<input type="tel" class="form-control" placeholder="Teléfono">
+							</div>
+						</div>
+						<p class="text-center">
+							<button class="btn btn-lg btn-secondary" type="submit">Publicar</button>
+						</p>
 					</form>
 				</div>
-	  	</div>
+			</div>
 		</div>
-  </div>
+  	</div>
 </div>
